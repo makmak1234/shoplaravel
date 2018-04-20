c4ab379c021ae11d5d76b30a45e1856484f769e9
makmak1234 <qwertyfamiliya@gmail.com>
2018-04-20 17:09:37 +0300

    translation with change tables add storage32

--- resources/views/frontend/subcat.blade.php
+++ resources/views/frontend/subcat.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.base')
 
 @section('stylesheets')
+    @parent
     {{-- <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"> --}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/frontend/indexUser.css')}}">
